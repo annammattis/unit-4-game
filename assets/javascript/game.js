fd096ae582693088b3b1e752b63bd9bfa7d81de4
annammattis <54290779+annammattis@users.noreply.github.com>
2019-11-14 17:00:17 -0500

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+var number = Math.round(Math.random() * 121) + 19;
+var crystalNumber = Math.round(Math.random() * 13) + 1;
+//create variables for wins, losses, # of guesses, letters guessed, reset
+var wins = 0;
+var losses = 0;
+// var numOfGuesses;
+// var displayWord = [];
+
+// function gameSetUp() {
+//   numOfGuesses = 15;
+//   randomWord = [Math.floor(Math.random() * words.length)];
+//   for (var i = 0; i < randomWord.length; i++) {
+//     displayWord.push('_');
+//   }
+//   console.log('randomWord', randomWord);
+//   console.log('displayWord', displayWord);
+
+//   document.getElementById('guessedWord').innerHTML = displayWord.join(' ');
+// }
+
+// gameSetUp();
