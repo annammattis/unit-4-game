93bed5a2b4889522dc4aed31b9a706e60b96011a
annammattis <54290779+annammattis@users.noreply.github.com>
2019-11-15 11:53:24 -0500

    update 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,21 @@
+$( document ).ready(function() {
+// allow for number listed to be between 19-120
 var number = Math.round(Math.random() * 121) + 19;
-var crystalNumber = Math.round(Math.random() * 13) + 1;
-//create variables for wins, losses, # of guesses, letters guessed, reset
+
+// allow for the 4 crystals to be valued between 1-12
+$('#crystals').text(Random);
+var crystal1 = Math.round(Math.random() * 13) + 1;
+var crystal2 = Math.round(Math.random() * 13) + 1;
+var crystal3 = Math.round(Math.random() * 13) + 1;
+var crystal4 = Math.round(Math.random() * 13) + 1;
+
+//create variables for wins, losses
 var wins = 0;
 var losses = 0;
+}
+
+
+
 // var numOfGuesses;
 // var displayWord = [];
 
